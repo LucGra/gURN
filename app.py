ab3f01c4cfb08920337f4098b27649adb9de511a
Lucas Gracia <lucas@Lucass-MacBook-Pro.local>
2023-01-28 21:21:34 +0000

    skeleton route handler for login created

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     if 'liveconsole' not in gethostname():  # check if running on pythonanywhere
         app.run()
